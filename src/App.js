9c98d5f234dc70ac4fe5b10852aa2812dd876e8d
APPsZone <35963951+APPsZone@users.noreply.github.com>
2021-08-26 15:22:36 +0700

    Change Header Component to Paper.JSX

--- src/App.js
+++ src/App.js
@@ -1,60 +1,60 @@
 import React from "react";
 import "./styles.css";
 
+import Paper from "./components/Paper";
+
 export default function App() {
   return (
-    <div className="container">
-      <div className="frame">
-        <section className="header">
-          <button className="header-btn main-black-color">Add</button>
-          <h1 className="header-title">To Do Lists</h1>
-          <button className="header-btn main-red-color">Clear</button>
-        </section>
+    <Paper>
+      <section className="header">
+        <button className="header-btn main-black-color">Add</button>
+        <h1 className="header-title">To Do Lists</h1>
+        <button className="header-btn main-red-color">Clear</button>
+      </section>
 
-        <section className="add">
-          <form className="add-form">
-            <input type="text" className="add-input" />
-            <button className="add-btn main-black-color">Add</button>
-          </form>
-        </section>
+      <section className="add">
+        <form className="add-form">
+          <input type="text" className="add-input" />
+          <button className="add-btn main-black-color">Add</button>
+        </form>
+      </section>
 
-        <section className="todos">
-          <div className="todo">
-            <span className="todo-text">React.js Project</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">React.js Project</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">React.js Project</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">React.js Project</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">React.js Project</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">React.js Project</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">React.js Project</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">React.js Project</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">React.js Project</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">React.js Project</span>
-          </div>
-        </section>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Bungee&family=Special+Elite&display=swap"
-          rel="stylesheet"
-        />
-      </div>
-    </div>
+      <section className="todos">
+        <div className="todo">
+          <span className="todo-text">React.js Project</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">React.js Project</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">React.js Project</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">React.js Project</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">React.js Project</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">React.js Project</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">React.js Project</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">React.js Project</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">React.js Project</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">React.js Project</span>
+        </div>
+      </section>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Bungee&family=Special+Elite&display=swap"
+        rel="stylesheet"
+      />
+    </Paper>
   );
 }

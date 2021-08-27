f162724a96a1742c13c7df66191e629ae5fa3b2e
APPsZone <35963951+APPsZone@users.noreply.github.com>
2021-08-27 15:16:31 +0700

    Make Form with Hooks (React.js)

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-const Form = () => {
+const Form = ({ addToDo }) => {
+  const [value, setValue] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!value) {
+      alert("Please fill up with the sentences.");
+      return;
+    }
+
+    addToDo(value);
+    alert(value);
+    setValue("");
+  };
+
   return (
     <section className="add">
-      <form className="add-form">
-        <input type="text" className="add-input" />
+      <form className="add-form" onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          className="add-input"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
         <button className="add-btn main-black-color">Add</button>
       </form>
     </section>
   );
+};
+
+Form.propTypes = {
+  addToDo: PropTypes.func.isRequired
 };
 
 export default Form;

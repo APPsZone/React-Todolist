da57a7a0df26e7fa20439fcc94e1ddfd9f204159
APPsZone <35963951+APPsZone@users.noreply.github.com>
2021-08-27 11:55:13 +0700

    Migrate Verbous to Components JSX

--- src/App.js
+++ src/App.js
@@ -2,55 +2,16 @@ import React from "react";
 import "./styles.css";
 
 import Paper from "./components/Paper";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import Todos from "./components/Todos";
 
 export default function App() {
   return (
     <Paper>
-      <section className="header">
-        <button className="header-btn main-black-color">Add</button>
-        <h1 className="header-title">To Do Lists</h1>
-        <button className="header-btn main-red-color">Clear</button>
-      </section>
-
-      <section className="add">
-        <form className="add-form">
-          <input type="text" className="add-input" />
-          <button className="add-btn main-black-color">Add</button>
-        </form>
-      </section>
-
-      <section className="todos">
-        <div className="todo">
-          <span className="todo-text">React.js Project</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">React.js Project</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">React.js Project</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">React.js Project</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">React.js Project</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">React.js Project</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">React.js Project</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">React.js Project</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">React.js Project</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">React.js Project</span>
-        </div>
-      </section>
+      <Header />
+      <Form />
+      <Todos />
       <link
         href="https://fonts.googleapis.com/css2?family=Bungee&family=Special+Elite&display=swap"
         rel="stylesheet"

42218504e9e214277be875587152b8d857b7c863
APPsZone <35963951+APPsZone@users.noreply.github.com>
2021-09-10 18:49:21 +0700

    Complete Todo Func

--- src/pages/Todolist.jsx
+++ src/pages/Todolist.jsx
@@ -7,28 +7,33 @@ import Todos from "../components/Todos";
 
 const Todolist = () => {
   const [todos, setTodos] = useState([
-    { text: "Learning React." },
-    { text: "Learning React Hooks." },
-    { text: "Learning Styling in React." }
+    { text: "Learning React.", isCompleted: false },
+    { text: "Learning React Hooks.", isCompleted: false },
+    { text: "Learning Styling in React.", isCompleted: false }
   ]);
 
   const [showAdd, setShowAdd] = useState(false);
 
   const addToDo = (value) => {
-    const addedTodo = [...todos, { text: value }];
+    const addedTodo = [...todos, { text: value, isCompleted: false }];
 
     setTodos(addedTodo);
   };
+  const completeTodo = (index) => {
+    const addedTodo = [...todos];
+    addedTodo[index].isCompleted = !addedTodo[index].isCompleted;
 
+    setTodos(addedTodo);
+  };
   const showAddToggle = () => setShowAdd(!showAdd);
 
-  console.log("showAdd", showAdd);
+  console.log("todos", todos);
 
   return (
     <Paper>
       <Header showAddToggle={showAddToggle} showAdd={showAdd} />
       <Form addToDo={addToDo} showAdd={showAdd} />
-      <Todos todos={todos} />
+      <Todos todos={todos} completeTodo={completeTodo} />
       <link
         href="https://fonts.googleapis.com/css2?family=Bungee&family=Special+Elite&display=swap"
         rel="stylesheet"

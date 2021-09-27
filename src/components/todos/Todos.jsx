9d7c69363c636f107ba2b54b8da2346ab1300567
APPsZone <35963951+APPsZone@users.noreply.github.com>
2021-09-28 00:32:30 +0700

    Refractor Form and Todo Components

--- src/components/todos/Todos.jsx
+++ src/components/todos/Todos.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Todo from "./Todo";
+import Todo from "../todo/Todo";
+
+import styles from "./todos.module.css";
 
 const Todos = ({ todos, completeTodo }) => {
   return (
-    <section className="todos">
+    <section className={styles.todos}>
       {todos.length > 0 &&
         todos.map((todo, index) => {
           return (
@@ -18,9 +20,9 @@ const Todos = ({ todos, completeTodo }) => {
           );
         })}
       {todos.length === 0 && (
-        <div className="todo-placeholder-text">
+        <div className={styles.todoPlaceholderText}>
           Add Todo by clicking{" "}
-          <span className="add-button-placeholder-text">Add</span> button on
+          <span className={styles.addButtonPlaceholderText}>Add</span> button on
           top-left corner.
         </div>
       )}
